e78d990bbd7f114eb2c6c16d472440a7e41d4e6b
Aditi Ray <imakeothersmile@gmail.com>
2023-09-19 10:42:27 +0530

    Factorial of a Number by Iteration

---
+++ Factorial_iteration.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int factorial(int a){
+    int fact=1;
+    for(int i=2; i<=a; i++){
+        fact*=i;
+    } 
+    return fact;
+}
+
+int main(){
+    int a;
+    printf("Enter a number ");
+    scanf("%d", &a);
+    printf("Factorial of %d is %d", a, factorial(a));
+}
